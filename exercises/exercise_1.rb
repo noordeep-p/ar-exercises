2431153f0732b3726949d8763ec85f7d12e5892d
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-12-31 00:08:49 -0500

    create stores model in db

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,9 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+Store.create(name: 'Burnaby', annual_revenue: 300000)
+Store.create(name: 'Richmond', annual_revenue: 1260000)
+Store.create(name: 'Gastown', annual_revenue: 190000)
+
+puts Store.all.count
