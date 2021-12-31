a7c77d7c5e02276c7be2e35377113de2bd89e44b
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-12-31 00:50:23 -0500

    add relational data inside model classes & create mock employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Noor", last_name: "Singh", hourly_rate: 40)
+@store1.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 50)
+
+@store2.employees.create(first_name: "Ron", last_name: "Sampang", hourly_rate: 40)
+@store2.employees.create(first_name: "Tom", last_name: "Zen", hourly_rate: 50)
+@store2.employees.create(first_name: "Gray", last_name: "Zurich", hourly_rate: 45)
