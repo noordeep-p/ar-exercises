6867e7405c0b2a0c42716bc2e8e5b3e6a4cced0f
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-12-31 01:18:22 -0500

    add validations to store method class

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,10 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, :greater_than => 0 }
+  def atleast_one_clothes_type
+    if mens_apparel.blank? && womens_apparel.blank?
+      errors.add(:atleast_one_clothes_type, "Stores must carry at least one of the men's or women's apparel" )
+    end
+  end
 end
